75b5438dc0954c155a942576871c1acef24fd6bc
lambt <lambt@vn-sis.com>
2024-11-04 17:27:47 +0700

    done register

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    public function register()
+    {
+        return view('auth.register');
+    }
+
+    public function store()
+    {
+        // dd("abc");
+        $validated = request()->validate([
+            'name' => 'required|min:3|max:40',
+            'email' => 'required|email|unique:users,email',
+            'password' => 'required|confirmed|min:8'
+        ]);
+
+        // dd("abc1");
+
+        User::create(
+            [
+                'name' => $validated['name'],
+                'email' => $validated['email'],
+                'password' => Hash::make($validated['password']),
+            ]
+        );
+
+        // User::create(
+        //     [
+        //         'name' => $validated['name'],
+        //         'email' => $validated['email'],
+        //         'password' => Hash::make($validated['password']),
+        //     ]
+        // );
+
+        return redirect()->route("dashboard")->with("success", "Account created Successfully!");
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\CommentController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\IdeaController;
 use App\Http\Controllers\ProfileController;
@@ -7,9 +9,21 @@ use App\Http\Controllers\TermsController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
+
 Route::get('/idea/{id}', [IdeaController::class, 'show'])->name('idea.show');
+
 Route::get('/idea/{id}/edit', [IdeaController::class, 'edit'])->name('idea.edit');
+
 Route::put('/idea/{id}', [IdeaController::class, 'update'])->name('idea.update');
+
 Route::post('/idea', [IdeaController::class, 'store'])->name('idea.create');
+
 Route::delete('/idea/{id}', [IdeaController::class, 'destroy'])->name('idea.destroy');
+
+Route::post('/ideas/{idea}/comments', [CommentController::class, 'store'])->name('ideas.comments.store');
+
 Route::get('/terms', [TermsController::class, 'index']);
+
+//Auth
+Route::get('/register', [AuthController::class, 'register'])->name('register');
+Route::post('/register', [AuthController::class, 'store']);
